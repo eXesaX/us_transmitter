1e29b8fa1fa0cf7da73ebf16a6d6bb1ea6d75cf9
exesax <exesa@yandex.ru>
2015-11-13 17:15:54 +0500

    fix charachter encoding

--- us_receiver_v2/us_receiver_v2.ino
+++ us_receiver_v2/us_receiver_v2.ino
@@ -1,5 +1,5 @@
 #define OUTPUT_INTERVAL 1000
-#define SIGNAL_GAP 80
+#define SIGNAL_GAP 200
 #define EOM_THRESHOLD 1000
 
 volatile long acc = 0;
@@ -7,7 +7,7 @@ volatile long lastFrontTime = 0;
 volatile int signalsReceived = 0;
 long IOTimer = 0;
 String message = "";
-
+long lFT = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(8, INPUT);
@@ -27,7 +27,7 @@ void onHigh() {
     signalsReceived += 1;
     message.concat('1');
 //    detectEOM();
-  } else if((millis() - lastFrontTime > (0.5*SIGNAL_GAP))) {
+  } else if((millis() - lastFrontTime > (0.3*SIGNAL_GAP))) {
     showMessage();
   }
   
@@ -49,9 +49,20 @@ void showMessage() {
 //  message.remove(message.length() - 1);
 //  Serial.println("Message: " + message);
   char str[8];
-  message.toCharArray(str, 8);
-  char symbol = GetBitArrayAsByte(str);
+//  message.toCharArray(str, 8);
+//  char symbol = GetBitArrayAsByte(str);
+  char symbol = 0;
+  for (int sym = 0; sym < message.length(); sym++) {
+    char bt = message.charAt(7-sym);
+    if (bt == '1') {
+      symbol |= (1 << sym);  
+    } else {
+      symbol |= (0 << sym);
+    }
+    
+  }
   Serial.print(symbol);
+//  Serial.println(symbol, BIN);
   message = "";
 //  message.concat(lastBit);
 }
@@ -61,16 +72,21 @@ char GetBitArrayAsByte(const char inputArray[8])
     char result = 0;
     for (int idx = 0; idx < 8; ++idx)
     {
-        result |= (inputArray[idx] << idx);
+        result |= (inputArray[7-idx] << idx);
     }
     return result;
 }
 void loop() {
-//  if ((millis() - IOTimer) > OUTPUT_INTERVAL) {
+
+//  Serial.println(millis() - lastFrontTime);
+
+  
+  
+  if ((millis() - IOTimer) > OUTPUT_INTERVAL) {
 //    Serial.print(signalsReceived);
 //    Serial.println(" " + message);
-//    IOTimer = millis();
-//  }
+    IOTimer = millis();
+  }
   
   
 }
--- us_sender_v2/us_sender_v2.ino
+++ us_sender_v2/us_sender_v2.ino
@@ -1,11 +1,12 @@
-#define TIME 12
-#define SIGNAL_GAP 130
+#define TIME 15
+#define SIGNAL_GAP 250
 
-String message = "Hello, world!\n";
+String message = "ABCDEF";
 
 void setup() {
   DDRB = B00000011;
   //pinMode(8, OUTPUT);
+  Serial.begin(115200);
 }
 
 void sendBits(String bits) {
@@ -14,17 +15,44 @@ void sendBits(String bits) {
       PORTB = B00000001;
       delayMicroseconds(TIME);
       PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
       delay(SIGNAL_GAP);  
     } else {
       PORTB = B00000001;
       delayMicroseconds(TIME);
       PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
       delay(1.4*SIGNAL_GAP);  
     }
     if (i == bits.length() - 1) {
       PORTB = B00000001;
       delayMicroseconds(TIME);
       PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
+      PORTB = B00000001;
+      delayMicroseconds(TIME);
+      PORTB = B00000000;
+      delayMicroseconds(TIME);
       delay(0.5*SIGNAL_GAP);
     }
   }
@@ -39,8 +67,15 @@ void sendBits(String bits) {
 }
 
 void loop() {
+  
   for (int num = 0; num < message.length(); num++) {
-    sendBits(String((byte)message.charAt(num), BIN));  
+    String bits = String(message.charAt(num), BIN);
+    for (int n = 0; n < 8 - bits.length(); n++) {
+      bits = "0" + bits;
+    }
+    Serial.println(bits);
+    sendBits(bits);
   }
+//  Serial.println(message);
   
 }
